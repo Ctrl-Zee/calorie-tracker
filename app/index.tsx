e298ce21cd1927c30576fa29b7c68cc0dbbd6c16
andrew.smith <andrew.smith.indy@gmail.com>
2024-12-04 09:14:41 -0500

    added logic to refresh the cache when app opens

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,38 @@
-import { useRef } from "react";
-import { StyleSheet } from "react-native";
+import { useEffect, useRef } from "react";
+import { AppState, StyleSheet } from "react-native";
 import BottomNavigation from "@/components/BottomNavigation";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import BottomSheet from "@gorhom/bottom-sheet";
 import AppBottomSheet from "@/components/AppBottomSheet";
 import DailyCalories from "@/components/DailyCalories";
 import { useTheme } from "react-native-paper";
+import { useQueryClient } from "@tanstack/react-query";
+import { EntryKeys } from "@/keys/QueryKeys";
 
 export default function Index() {
   const bottomSheetRef = useRef<BottomSheet>(null);
   const theme = useTheme();
+  const queryClient = useQueryClient();
 
   const handleBottomSheetOnOpen = () => {
     bottomSheetRef.current?.snapToIndex(1);
   };
+
+  // Invalidate the entries query when the app is opened.
+  useEffect(() => {
+    const subscription = AppState.addEventListener("change", (nextAppState) => {
+      if (nextAppState === "active") {
+        queryClient.invalidateQueries({
+          queryKey: EntryKeys.all,
+        });
+      }
+    });
+
+    // Clean up subscription on unmount
+    return () => {
+      subscription.remove();
+    };
+  }, []);
 
   return (
     <GestureHandlerRootView
