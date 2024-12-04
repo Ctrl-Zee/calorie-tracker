509d14a32569c1d093b908f8aa491046064edd5f
andrew.smith <andrew.smith.indy@gmail.com>
2024-12-04 09:15:05 -0500

    added drawer for meal types

--- components/CalorieLogger.tsx
+++ components/CalorieLogger.tsx
@@ -5,11 +5,13 @@ import { CalorieEntry, MealType } from "@/types/CalorieEntry";
 import { useQueryClient } from "@tanstack/react-query";
 import { EntryKeys } from "@/keys/QueryKeys";
 import SheetButton from "@/components/SheetButton";
-import { TextInput } from "react-native-paper";
+import { TextInput, Text, useTheme } from "react-native-paper";
 import MealTypeOptions from "@/components/MealTypeOptions";
 import "react-native-get-random-values";
 import { v4 as uuidv4 } from "uuid";
 import { getMealByTime } from "@/utils/MealByTime";
+import Animated, { FadeInUp, FadeOutDown } from "react-native-reanimated";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 /**
  * Handles the input for the calorie logger.
@@ -19,8 +21,10 @@ const CalorieLogger = () => {
   const [selectedMealType, setSelectedMealType] = useState<MealType | null>(
     getMealByTime(), // TODO: set the meal type when the bottom sheet is opened.
   );
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
   const { mutate: addCalories } = useAddEntry();
   const queryClient = useQueryClient();
+  const theme = useTheme();
 
   const handleSave = () => {
     const entry = {
@@ -53,14 +57,35 @@ const CalorieLogger = () => {
     <View style={styles.container}>
       <View style={styles.header}>
         <SheetButton icon="close" onPress={resetInput} />
+        <View style={styles.mealTypeButtonContainer}>
+          <Text
+            variant="titleLarge"
+            style={styles.capitalize}
+            onPress={() => setIsDrawerOpen(!isDrawerOpen)}
+          >
+            {selectedMealType}
+          </Text>
+          <AntDesign
+            name="caretdown"
+            size={14}
+            color={theme.colors.onSurface}
+          />
+        </View>
         <SheetButton icon={"check"} onPress={handleSave} />
       </View>
-      <View style={styles.mealTypeContainer}>
-        <MealTypeOptions
-          selectedMealType={selectedMealType}
-          setSelectedMealType={setSelectedMealType}
-        />
-      </View>
+
+      {isDrawerOpen && (
+        <Animated.View
+          style={[styles.mealTypeContainer]}
+          entering={FadeInUp}
+          exiting={FadeOutDown}
+        >
+          <MealTypeOptions
+            selectedMealType={selectedMealType}
+            setSelectedMealType={setSelectedMealType}
+          />
+        </Animated.View>
+      )}
       <View style={styles.inputContainer}>
         <TextInput
           value={calories}
@@ -84,16 +109,26 @@ const styles = StyleSheet.create({
   header: {
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
     width: "100%",
     height: 50,
     marginBottom: 20,
   },
   mealTypeContainer: {
     width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
     marginBottom: 20,
   },
+  mealTypeButtonContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
   inputContainer: {
-    width: "80%",
+    width: "85%",
+  },
+  capitalize: {
+    textTransform: "capitalize",
   },
 });
 
